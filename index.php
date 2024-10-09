8bfaab754160817ae624cac20d61de6a801e3325
Markamodia12 <167683273+Markamodia12@users.noreply.github.com>
2024-10-09 23:52:35 +0800

    Update index.php

--- index.php
+++ index.php
@@ -15,66 +15,80 @@
 <body>
 <?php 
 
-$stmt = $pdo->prepare("SELECT * FROM Guests");
+//$stmt = $pdo->prepare("SELECT * FROM Guests");
 
-if ($stmt->execute()) {
-     echo "string"; "<pre>";
-     print_r($stmt -> fetchALL());
-     echo"<pre>";
+//if ($stmt->execute()) {
+     //echo "string"; "<pre>";
+     //print_r($stmt -> fetchALL());
+     //echo"<pre>";
 
- $stmt = $pdo->prepare("SELECT * FROM Reservations WHERE reservation_id = 1");
+ //$stmt = $pdo->prepare("SELECT * FROM Reservations WHERE reservation_id = 1");
 
-        if ($stmt->execute()) {
-            echo "<pre>";
-            print_r($stmt->fetch());
-            echo "</pre>";
-            }
+        //if ($stmt->execute()) {
+            //echo "<pre>";
+            //print_r($stmt->fetch());
+            //echo "</pre>";
+            //}
 
-$query = "INSERT INTO Guests(guest_id, first_name, last_name, email, phone_number, address) values(?,?,?)";
+//$query = "INSERT INTO Guests(guest_id, first_name, last_name, email, phone_number, address) values(?,?,?)";
 
-$stmt = $pdo->prepare($query);
+//$stmt = $pdo->prepare($query);
 
-$executeQuery = $stmt->execute(
-        [2,"Jane","Smith"]
-   );
+//$executeQuery = $stmt->execute(
+        //[2,"Jane","Smith"]
+   //);
 
-    if ($executeQuery) {
+    //if ($executeQuery) {
         echo "Query successful!";
-   };
-    else {
-        echo "Query failed";
-    }
+   //};
+    //else {
+        //echo "Query failed";
+    //}
 
- $query = "DELETE FROM Reservations
+ //$query = "DELETE FROM Reservations
              // WHERE reservation_id = 6 ";
     
-     $stmt = $pdo->prepare($query);
+     //$stmt = $pdo->prepare($query);
     
-     $executeQuery = $stmt->execute();
+     //$executeQuery = $stmt->execute();
     
-     if ($executeQuery) {
-        echo "Deletion successful!";
-     }
-     else {
-     echo "Query failed";      
+     //if ($executeQuery) {
+        //echo "Deletion successful!";
+     //}
+     //else {
+     //echo "Query failed";      
 
-$query ="UPDATE Guests
-         SET first_name = ?, last_name = ?
-         WHERE id = 1";
+//$query ="UPDATE Guests
+         //SET first_name = ?, last_name = ?
+         //WHERE id = 1";
 
-$stmt = $pdo->prepare($query);
+//$stmt = $pdo->prepare($query);
 
-$executeQuery = $stmt->execute(
-  ["Mark", "Amodia"]
-);
+//$executeQuery = $stmt->execute(
+  //["Mark", "Amodia"]
+//);
 
-if ($executeQuery)    {
-    echo "udpate successful!"
-}
+//if ($executeQuery)    {
+    //echo "udpate successful!"
+//}
 
-else  {
-    echo "Query failed";
-}
-
+//else  {
+    //echo "Query failed";
+//}
+ ?>
+    
+    <table>
+        <tr>
+            <th>Reservation</th>
+            <th>Services</th>
+        </tr>
+        <?php foreach ($Services as $row) { ?>
+        <tr>
+            <td><?php echo $row['service_id']; ?></td>
+            <td><?php echo $row['service_name']; ?></td>
+            <td><?php echo $row['service_price']; ?></td>
+        </tr>
+        <?php } ?>
+    </table> 
 </body>
 </html>
